bbd618dbc2ff8c7dbea0050cff2af1ffd44454ca
granth <granthbagadia2004@gmail.com>
2024-01-28 02:20:20 +0530

    push

--- app.py
+++ app.py
@@ -1,6 +1,61 @@
 from flask import Flask, render_template, request, redirect, url_for
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+import google.auth.transport.requests
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+client_secrets_file = os.path.join(
+    pathlib.Path(__file__).parent, "oauth.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri=os.getenv("CALLBACK")
+)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    "DocString"
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    "DocString"
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(
+        session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+    print(id_info)
+    if 'bits-pilani.ac.in' not in id_info['email']:
+        session.clear()
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    "DocString"
+    session.clear()
+    return redirect("/")
 
 
 @app.route('/')
